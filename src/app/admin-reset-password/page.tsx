5f9a3c580798e602bf7bbc94de29896da39bf799
Hackiodtech <geddyprime@gmail.com>
2025-12-05 12:47:39 +0100

    d

--- src/app/admin-reset-password/page.tsx
+++ src/app/admin-reset-password/page.tsx
@@ -1,10 +1,10 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
-// Load the actual client page without SSR
-const AdminResetPasswordPage = dynamic(
-  () => import("./reset-form"),
-  { ssr: false }
-);
+const AdminResetPasswordPage = dynamic(() => import("./reset-form"), {
+  ssr: false,
+});
 
 export default function Page() {
   return <AdminResetPasswordPage />;
