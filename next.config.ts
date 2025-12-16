78b3d36241eedea8eb4c8aec303c328958ff83a4
Jharmez007 <jamesagu2001@gmail.com>
2025-12-16 13:25:31 +0100

    confignext

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,18 @@ import type { NextConfig } from "next";
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const nextConfig: NextConfig = {
-  images: backendUrl
-    ? {
-        remotePatterns: [
-          {
-            protocol: backendUrl.startsWith("https") ? "https" : "http",
-            hostname: new URL(backendUrl).hostname,
-            pathname: "/**",
-          },
-        ],
-      }
-    : undefined,
+  images:
+    backendUrl && backendUrl.startsWith("http")
+      ? {
+          remotePatterns: [
+            {
+              protocol: backendUrl.startsWith("https") ? "https" : "http",
+              hostname: new URL(backendUrl).hostname,
+              pathname: "/",
+            },
+          ],
+        }
+      : undefined,
 };
 
 export default nextConfig;
